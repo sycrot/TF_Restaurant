8f26960fc68e87f58b84b2225b37d3021c5d76b0
Thiago Farias <thiagofarias99@hotmail.com>
2020-04-03 17:26:52 -0300

    Reservando uma mesa

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var conn = require('./../inc/db');
 var express = require('express');
 var menus = require('./../inc/menus');
+var reservation = require('./../inc/reservation');
 var router = express.Router();
 
 /* GET home page. */
@@ -46,12 +47,32 @@ router.get('/menus', function(req, res, next) {
 
 router.get('/reservations', function(req, res, next) {
 
-  res.render('reservation', {
-    title: 'Reservas - TF Restaurant',
-    background: 'images/img_bg_2.jpg',
-    h1: 'Reserve uma mesa!',
-    isHome: false
-  });
+  reservation.render(req, res);
+
+});
+
+router.post('/reservations', function(req, res, next) {
+
+  if (!req.body.name) {
+    reservation.render(req, res, "Digite o nome");
+  } else if (!req.body.email) {
+    reservation.render(req, res, "Digite o e-mail");
+  }else if (!req.body.people) {
+    reservation.render(req, res, "Digite o número de pessoas");
+  }else if (!req.body.date) {
+    reservation.render(req, res, "Digite a data");
+  }else if (!req.body.time) {
+    reservation.render(req, res, "Digite o horario");
+  } else {
+    
+    reservation.save(req.body).then(results => {
+      req.body = {};
+      reservation.render(req, res, null, "Reserva realizada com sucesso!");
+    }).catch(err => {
+      reservation.render(req, res, err.message);
+    });
+
+  }
 
 });
 
