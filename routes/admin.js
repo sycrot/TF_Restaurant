cdbff68bc99601cb0133eb262a737bd6763a94b5
Thiago Farias <thiagofarias99@hotmail.com>
2020-04-04 18:09:15 -0300

    Tela de de menus admin exibindo o cardápio

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,52 @@
 var express = require('express');
 var user = require('./../inc/admin/users');
+var admin = require('./../inc/admin/admin');
+var menus = require('./../inc/menus');
 var router = express.Router();
+
+router.use(function(req, res, next) {
+
+    if (['/login'].indexOf(req.url) === -1 && !req.session.user) {
+        res.redirect('/admin/login');
+    } else {
+        next();
+    }
+
+});
+
+router.use(function(req, res, next){
+
+    req.menus = admin.getMenus(req);
+
+    next();
+
+});
+
+router.get('/logout', function(req, res, next){
+
+    delete req.session.user;
+    res.redirect('/admin/login');
+
+});
 
 router.get("/", function(req, res, next){
 
-    res.render('admin/index', {
-        
+    admin.dashboard().then(data => {
+
+        res.render('admin/index', admin.getParams(req, {
+            data
+        }));
+
+    }).catch(err => {
+        console.error(err);
     });
+
 
 });
 
 router.get("/contacts", function(req, res, next){
 
-    res.render('admin/contacts', {
-        
-    });
+    res.render('admin/contacts', admin.getParams(req));
 
 });
 
@@ -50,33 +82,46 @@ router.get("/login", function(req, res, next){
 
 router.get("/emails", function(req, res, next){
 
-    res.render('admin/emails', {
-        
-    });
+    res.render('admin/emails', admin.getParams(req));
 
 });
 
+// --menus
 router.get("/menus", function(req, res, next){
 
-    res.render('admin/menus', {
-        
+    menus.getMenus().then(data => {
+
+        res.render('admin/menus', admin.getParams(req, {
+            data
+        }));
+    
     });
 
 });
+
+router.post("/menus", function(req, res, next) {
+
+    menus.save(req.fields, req.files).then(results=> {
+        res.send(results);
+    }).catch(err=>{
+        res.send(err);
+    });
+
+});
+// --||
 
 router.get("/reservations", function(req, res, next){
 
-    res.render('admin/reservations', {
+    res.render('admin/reservations', admin.getParams(req, {
         date:{}
-    });
+    }));
 
 });
 
+
 router.get("/users", function(req, res, next){
 
-    res.render('admin/users', {
-        
-    });
+    res.render('admin/users', admin.getParams(req));
 
 });
 
