964e9c0c07f6eb80257162784d067c2f62bd0970
Thiago Farias <thiagofarias99@hotmail.com>
2020-04-11 11:21:34 -0300

    Criado component grid / erro ao editar or excluir

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -1,191 +1,100 @@
-class HcodeGrid {
+class Grid {
 
-    constructor(config) {
-
-        config.listeners = Object.assign({
-            afterUpdateClick() {
-
-                $(this.options.modalUpdate).modal('show');
-
-            },
-            afterDeleteClick() {
-
-                window.location.reload();
-
-            },
-            afterFormCreate(){
-
-                window.location.reload();
-
-            },
-            afterFormCreateError(){
-
-                alert('Não foi possível enviar o formulário!');
-
-            },
-            afterFormUpdate() {
-
-                window.location.reload();
-
-            },
-            afterFormUpdateError() {
-
-                alert('Não foi possível enviar o formulário!');
-
-            },
-            clickRowButton(btn, row, data, e) {
-                console.info('clickRowButton', btn, row, data, e);
-            }
-        }, config.listeners);
+    constructor(configs) {
 
         this.options = Object.assign({}, {
-            modalCreate: '#modal-create',
-            modalUpdate: '#modal-update',
-            btnUpdate: '.btn-update',
-            btnDelete: '.btn-delete',
-            textDeleteConfirm: 'Deseja realmente excluir?',
-            onUpdateLoad: (formUpdate, name, data) => {
+            formCreate:'#modal-create form',
+            formUpdate:'#modal-update form',
+            btnUpdate:'.btn-update',
+            btnDelete:'.btn-delete',
+        }, configs);
 
+        this.init();
+        this.initButtons();
+
+    }
+
+    init() {
+        // create data
+        this.formCreate = document.querySelector(this.options.formCreate);
+
+        this.formCreate.save().then(json=> {
+
+        window.location.reload();
+
+        }).catch(err=>{
+        console.log(err);
+        });
+
+        // update data
+        this.formUpdate = document.querySelector(this.options.formUpdate);
+
+        this.formUpdate.save().then(json=> {
+
+        window.location.reload();
+
+        }).catch(err=>{
+        console.log(err);
+        });
+    }
+
+    initButtons() {
+        // update data
+        [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
+            btn.addEventListener('click', e => {
+            let tr = e.path.find(el => {
+
+                return (el.tagName.toUpperCase() === 'TR');
+
+            });
+
+            this.data = JSON.parse(tr.dataset.row);
+
+            for (let name in data) {
+                
                 let input = formUpdate.querySelector(`[name=${name}]`);
 
-                if (input) {
-                    switch (input.type) {
-                        case 'date':
-                            input.value = moment(data[name]).format('YYYY-MM-DD');
-                            break;
-                        default:
-                            input.value = data[name];
+                switch (name) {
+                
+                case 'date':
+                    if (input) input.value = moment(data[name]).format('YYYY-MM-DD');
+                break;
 
-                    }
+                default:
+                    if (input) input.value = data[name];
                 }
-
             }
-        }, config);
 
-        this.rows = [...document.querySelectorAll(`#${this.options.id} tbody tr`)];
+            $('#modal-update').modal('show');
 
-        this.formCreate = document.querySelector(this.options.modalCreate + ' form');
-        this.formUpdate = document.querySelector(this.options.modalUpdate + ' form');
+            });
+        });
 
-        this.initForms();
-        this.initRowButtons();
+        [...document.querySelectorAll(this.options.btnDelete)].forEach(btn=>{
 
-    }
+        btn.addEventListener('click', e=> {
 
-    fireEvent(name, args) {
+            let tr = e.path.find(el => {
 
-        if (typeof this.options.listeners[name] === 'function') this.options.listeners[name].apply(this, args);
-
-    }
-
-    getTrData(event) {
-
-        let tr = event.path.find(el => {
             return (el.tagName.toUpperCase() === 'TR');
-        });
-
-        return JSON.parse(tr.dataset.row);
-
-    }
-
-    initForms() {
-
-        if (this.formCreate) {
-            this.formCreate.submitAjax({
-                success: response => {
-                    this.fireEvent('afterFormCreate', [response]);
-                },
-                failure: () => {
-                    this.fireEvent('afterFormCreateError');
-                }
-            });
-        }
-
-        if (this.formUpdate) {
-            this.formUpdate.submitAjax({
-                success: response => {
-                    this.fireEvent('afterFormUpdate', [response]);
-                },
-                failure: () => {
-                    this.fireEvent('afterFormUpdateError');
-                }
-            });
-        }
-
-    }
-
-    initRowButtons() {
-
-        this.rows.forEach(row => {
-
-            [...row.querySelectorAll('.btn')].forEach(btn => {
-
-                btn.addEventListener('click', e => {
-
-                    if (btn.classList.contains('btn-update')) {
-
-                        this.actionBtnUpdate(e);
-
-                    } else if (btn.classList.contains('btn-delete')) {
-
-                        this.actionBtnDelete(e);
-
-                    } else {
-
-                        this.options.listeners.clickRowButton(btn, this.getTrData(e), row, e);
-
-                    }
-
-                });
 
             });
 
-        });
+            let data = JSON.parse(tr.dataset.row);
 
-    }
+            if (confirm(eval('`'+this.options.deleteMsg+'`'))) {
+            fetch(eval('`'+this.options.deleteUrl+'`'), {
+            method: 'DELETE'
+            }).then(response => response.json())
+            .then(json => {
+                
+                window.location.reload();
 
-    actionBtnUpdate(e) {
-
-        this.fireEvent('beforeUpdateClick');
-
-        let data = this.getTrData(e);
-
-        for (let name in data) {
-
-            this.options.onUpdateLoad(this.formUpdate, name, data);
-
-        }
-
-        this.fireEvent('afterUpdateClick');
-
-    }
-
-    actionBtnDelete(e) {
-
-        this.fireEvent('beforeDeleteClick');
-
-        let data = this.getTrData(e);
-
-        if (confirm(eval("`" + this.options.textDeleteConfirm + "`"))) {
-
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('DELETE', eval("`" + this.options.urlDelete + "`"), true);
-
-            xhr.onreadystatechange = response => {
-
-                if (xhr.readyState === 4 && xhr.status === 200) {
-
-                    this.fireEvent('afterDeleteClick');
-
-                }
-
+            });
             }
 
-            xhr.send();
+        });
 
-        }
-
+        });
     }
-
 }
